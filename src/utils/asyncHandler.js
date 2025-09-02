bfac52ae0f01a998b273eee9a89c4d95eaa308c3
Rohit Barma <rohit@example.com>
2025-09-02 14:12:07 +0530

    setup done

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,26 @@
+const asyncHandler = (requesthandler) => {
+    (req, res, next) => {
+        Promise.resolve(requesthandler(req, res, next))
+        .reject((err) => next(err))
+    }
+}
+
+
+export {asyncHandler}
+
+
+/*  // this is try catch method but many time we use promises to handle it
+// we user higher order function ok
+const asyncHandler = (fn) => async(req, res, next) => {
+    try{
+        await fn(req, res, next)
+    } catch (error) {
+        res.status(err.code || 500).json({
+            sucess: false,
+            message: err.message
+        })
+    }
+}
+
+export {asyncHandler}
+*/
